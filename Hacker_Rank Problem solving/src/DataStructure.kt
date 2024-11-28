e524ab029c4e17442b90877ebe84e02ce72a17c8
almuheetushihab <almuheetu@gmail.com>
2024-11-29 00:32:19 +0600

    resolved second Problem in DataStructure

--- Hacker_Rank Problem solving/src/DataStructure.kt
+++ Hacker_Rank Problem solving/src/DataStructure.kt
@@ -1,17 +1,23 @@
-
-
 //1st Number of DataStructure Problem Solve
 
 fun reverseArray(a: Array<Int>): Array<Int> {
     return a.reversedArray()
 }
 
-fun main(args: Array<String>) {
-    val arrCount = readln().trim().toInt()
 
-    val arr = readln().trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+//2nd Number of DataStructure Problem Solve
 
-    val res = reverseArray(arr)
+fun hourglassSum(arr: Array<Array<Int>>): Int {
+    var maxSum = Int.MIN_VALUE
+    for (i in 0..3) {
+        for (j in 0..3) {
+            val sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                    arr[i + 1][j + 1] +
+                    arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
 
-    println(res.joinToString(" "))
+            maxSum = maxOf(maxSum, sum)
+        }
+    }
+    return maxSum
 }
+
