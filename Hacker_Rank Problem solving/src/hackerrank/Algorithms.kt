49d95187c127d04d52ec14ca2d7031aee72acc4b
Almuheetu shihab <almuheetu@gmail.com>
2025-05-29 23:18:25 +0600

    resolvd: another problem

--- Hacker_Rank Problem solving/src/hackerrank/Algorithms.kt
+++ Hacker_Rank Problem solving/src/hackerrank/Algorithms.kt
@@ -132,7 +132,15 @@ fun pageCount(n: Int, p: Int): Int {
 //Counting Valleys
 //=>
 
+/*Drawing Bookxx*/
+//=>
+fun pageCounot(n: Int, p: Int): Int {
+    val fromFront = p / 2
 
+    val fromBack = n / 2 - p / 2
+
+    return minOf(fromFront, fromBack)
+}
 
 
 
