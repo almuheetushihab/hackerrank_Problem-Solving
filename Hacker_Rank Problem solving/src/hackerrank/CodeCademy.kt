15871367684371659d57c6a02e7c989f0bcd4cb2
Almuheetu shihab <almuheetu@gmail.com>
2025-06-26 11:41:31 +0600

    solved: Number Steps

---
+++ Hacker_Rank Problem solving/src/hackerrank/CodeCademy.kt
@@ -0,0 +1,12 @@
+package hackerrank
+
+//1 st number off problem-solving
+//Number steps
+//Many programming languages have built-in functions that output sequences of numbers separated by steps.
+// Create your own Kotlin function from scratch that creates a sequenced list of numbers from 0 to a maximum input value and input step.
+// For example, a maximum value of 8 and a step value of 2 should return an output of [2, 4, 6, 8].
+//
+//If you’re looking for more of a challenge, create a Fibonacci sequence generator.
+// Remember that the following number of a Fibonacci sequence is the sum of the two preceding it: [1, 1, 2, 3, 5, 8, 13].
+
+//=>
